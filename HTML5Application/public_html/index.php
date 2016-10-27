8aced37e608db4b973307a29d34583c676ac58e9
Melissa Case <Case-Melissa@users.noreply.github.com>
2016-10-27 12:43:40 -0700

    Add files via upload

---
+++ HTML5Application/public_html/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+This is my home page
+-->
+<html lang ="en-us">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home | Budgetdesigning.com</title>
+        <meta name="Author" content="Melissa Case">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="mycss.css">
+    </head>
+    <body id="home">
+        <div>
+            <header>
+
+            </header>
+        </div>
+        <main>
+            <div>
+                <h1 id="Home">Home</h1>
+                <p>Welcome to my site. Please click on the page for a working sample of my code.</p>   
+            </div>
+            <div>
+                <a href="public_html/ajax/ajax.html">Ajax</a><br>
+                <a href="public_html/arrays.html">Arrays</a><br>
+                <a href="public_html/associative.html">Associative</a><br>
+                <a href="public_html/html5/audio.html">Audio</a><br>
+                <a href="public_html/html5/canvas.html">Canvas</a><br>
+                <a href="public_html/cs.html">Conditional Statements</a><br>
+                <a href="public_html/dom.html">DOM</a><br>
+                <a href="public_html/inheritance.html">Inheritance</a><br>
+                <a href="public_html/loops.html">Loops</a><br>
+                <a href="public_html/local.html">Local Storage API</a><br>
+                <a href="public_html/properties.html">Properties</a><br>
+                <a href="public_html/stringify.html">Stringify</a><br>
+                <a href="public_html/html5/video.html">Video</a><br>
+                
+
+            </div>
+        </main>
+
+        <div>
+            <footer>
+
+            </footer>
+        </div>
+    </body>
+</html>
